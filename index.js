a2ce13ff5c66b66c4b51c1d28bb735826ebab73d
alicia <alicepai2020@gmail.com>
2021-05-20 18:24:52 +0800

    first push

---
+++ index.js
@@ -0,0 +1,44 @@
+const emailInput = document.querySelector("#email");
+const pwdInput = document.querySelector("#pwd");
+const loginBtn = document.querySelector("#loginbtn");
+
+const url = 'https://vue3-course-api.hexschool.io/'; 
+const path = 'dessert-alicia'; 
+
+function Login(){
+    //1.check ifboth input are not empty
+    if(emailInput.value == '' || pwdInput.value == ''){
+            swal({
+        title: "",
+        text: "帳號、密碼不得為空",
+        });
+        return;
+    }
+    const username = emailInput.value;
+    const password = pwdInput.value;
+    let user = {
+        username,
+        password
+    }
+    axios.post(`${url}admin/signin`,user)
+    .then(res=>{
+        console.log(res);
+        if(res.data.message == '登入成功'){
+            swal("登入成功", "3秒後跳轉產品頁","success")
+            setTimeout(()=>{
+                window.location.href=  './productList.html';
+            },3000)
+        }else{
+            swal("登入失敗",`${res.data.error.message}` ,"error")
+        }
+        const token = res.data.token;
+        const expired = res.data.expired;
+        console.log(token, expired);
+        //儲存 Token
+        document.cookie = `aliciaToken=${token}; expires =${new Date(expired)} `;
+
+    })
+}
+
+
+loginBtn.addEventListener('click',Login);
---
+++ productList.js
@@ -0,0 +1,74 @@
+const logoutBtn = document.querySelector('#logout')
+const productList  = document.querySelector("tbody");
+const productNum = document.getElementById("product_num");
+
+const url = 'https://vue3-course-api.hexschool.io/'; 
+const path = 'dessert-alicia'; 
+
+const app = {
+    data: {
+          products: []
+      },
+      getData(){
+        const token = document.cookie.replace(/(?:(?:^|.*;\s*)aliciaToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        axios.defaults.headers.common['Authorization'] = token;
+        axios.get(`${url}api/${path}/admin/products`)
+        .then(res=>{
+           console.log(res) 
+            if(res.data.success){
+  
+                this.data.products = res.data.products;
+                this.renderPage(this.data.products)
+            }
+        })
+      },
+      removeProduct(e){
+        const id =  e.target.dataset.id;
+        axios.delete(`${url}api/${path}/admin/product/${id}`)
+        .then(res=>{
+            swal("刪除成功",`` ,"success");
+            app.getData()});
+        },
+        renderPage(data){
+            let str = '';
+            let count = 0
+            for(let [key,value] of Object.entries(data)){
+                count++;
+                str += `
+                <tr>
+                <td>${value.title}</td>
+                <td>${value.origin_price}</td>
+                <td>${value.price}</td>
+                <td>${value.is_enabled? '啟用':'未啟用'}</td>
+                <td>
+                    <button type="button" class="btn btn-danger del" data-id=${value.id} data-action="remove">刪除</button>
+                </td>
+            </tr>
+            `
+            }
+            productList.innerHTML = str;
+            productNum.innerHTML = count;
+
+            const delBtns = document.querySelectorAll(".del");
+            delBtns.forEach(btn=>{
+                btn.addEventListener('click',app.removeProduct)
+            })
+        },
+        Logout(){
+            axios.post(`${url}logout`)
+            .then(res=>{
+                swal("登出成功",`三秒後返回登入頁面` ,"success");
+                delete axios.defaults.headers.common["Authorization"];
+                setTimeout(()=>{
+                    window.location.href="./index.html"
+                },3000)
+            })
+        }
+      }
+      app.getData();
+      productList.addEventListener('click',app.judgeAction)
+      logoutBtn.addEventListener('click',app.Logout);
+
+
+
+
